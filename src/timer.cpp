653965f06749ccac8c8798586e79afb12e554f55
Riley <100080465+CosmcBeta@users.noreply.github.com>
2025-10-30 22:32:35 -0500

    changed function to const

--- src/timer.cpp
+++ src/timer.cpp
@@ -12,7 +12,7 @@ void Timer::stop()
     endTime_ = std::chrono::system_clock::now();
 }
 
-int Timer::elapsed()
+int Timer::elapsed() const
 {
     long long elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(endTime_ -  startTime_).count();
     return static_cast<int>(elapsedTime);
--- src/timer.hpp
+++ src/timer.hpp
@@ -9,7 +9,7 @@ public:
 
     void start();
     void stop();
-    int elapsed();
+    int elapsed() const;
 
 private:
     std::chrono::time_point<std::chrono::system_clock> startTime_, endTime_;
