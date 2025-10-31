dd0c212cf94c9a34ffc9bf3ca4d75af01d08fa0c
Riley <100080465+CosmcBeta@users.noreply.github.com>
2025-10-30 22:31:32 -0500

    removed old comments

--- src/timer.cpp
+++ src/timer.cpp
@@ -4,11 +4,6 @@
 
 void Timer::start()
 {
-    // auto now = std::chrono::system_clock::now();
-    // auto nowSeconds = std::chrono::time_point_cast<std::chrono::seconds>(now);
-    // auto epoch = nowSeconds.time_since_epoch();
-    // auto value = std::chrono::duration_cast<std::chrono::seconds>(epoch);
-    // long duration = value.count();
     startTime_ = std::chrono::system_clock::now();
 }
 
@@ -19,20 +14,6 @@ void Timer::stop()
 
 int Timer::elapsed()
 {
-    // auto timeElapsed {endTime_ - startTime_};
-
     long long elapsedTime = std::chrono::duration_cast<std::chrono::seconds>(endTime_ -  startTime_).count();
-
-
-
-    // long long seconds_since_epoch = std::chrono::duration_cast<std::chrono::seconds>(timeElapsed.time_since_epoch()).count();
-
-
-
-
-    // If you are certain the value fits within an int:
-    // int int_seconds_since_epoch = static_cast<int>(seconds_since_epoch);
-
-
     return static_cast<int>(elapsedTime);
 }
